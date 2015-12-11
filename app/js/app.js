f0a5d6d37486c3b23fc686104135a78bfc541e02
aruanfreedom <aruanfreedom@outlook.com>
2015-12-11 13:12:58 +0600

    git ignore
    ignore File

--- app/js/app.js
+++ app/js/app.js
@@ -1,35 +1,68 @@
-//$(document).ready(function() {
-//    function animateText(id, text, i) {
-//        document.getElementById(id).innerHTML = text.substring(0, i);
-//        i++;
-//        set();
-//        if (i > text.length) {
-//            i = 0;
-//
-//        }else if( i === text.length){
-//            clearTimeout(set());
-//        }
-//
-//    function set(){
-//            setTimeout("animateText('" + id + "','" + text + "'," + i + ")", 200);
-//        }
-//
-//    }
-//
-//    function animLoad(){
-//        animateText("dynamic-text", "АРЕНДА АВТО ", 0);
-//    }
-//
-//    setTimeout(animLoad, 2000);
 
+    function animateText(id, text, i) {
+        document.getElementById(id).innerHTML = text.substring(0, i);
+        i++;
+        set();
+        if (i > text.length) {
+            i = 0;
 
-//});
+        }else if( i === text.length){
+            clearTimeout(set());
+        }
+
+    function set(){
+            setTimeout("animateText('" + id + "','" + text + "'," + i + ")", 600);
+        }
+
+    }
+
+    function animLoad(){
+        animateText("dynamic-text", "АРЕНДА АВТО ", 0);
+    }
+
+    setTimeout(animLoad, 3000);
+
 
 $(document).ready(function() {
+
+    var animationBg = function(){
+      $("#anim").animate({
+              height: 0
+      }, 2000, function(){
+          $(this).css('display', 'none')
+      });
+    };
+
+    //setTimeout(animationBg, 10000);
+
+    var afterAnimBg = function(){
+      var pages = ['menu', 'about'],
+          i;
+        for(i = 0; pages.length > i; i++){
+            $('#' + pages[i]).show();
+        }
+    };
+
+    //setTimeout(afterAnimBg, 9000);
+
     var animText = function(){
         $("#bisnesman-1 h3, span").css("opacity", 1);
-        console.log("ok")
-    }
-    setTimeout(animText, 6000);
+    };
+
+    //setTimeout(animText, 12000);
+
+    var logoAnim = function() {
+        var logo = parseInt( $("#logo").height()),
+            shine = $("#logo").before();
+
+        shine.css('display', 'none');
+    };
+
+    //logoAnim();
+
 });
+
+
+
+
 
